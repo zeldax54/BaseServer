36592eb986c3439c5eff50c18687d3bfe41233f7
zeldax <zeldax54@gmail.com>
2019-11-03 23:18:04 -0300

    weather info

---
+++ BaseServer/BaseServer.Web/Modules/FrontPage/FrontPageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Hepers;
+
+namespace BaseServer.FrontPage.Model
+{
+    public class FrontPageModel
+    {
+        public IpResponse IpResponse { get; set; }
+        public WheatherResponse WheatherResponse { get; set; }
+
+
+    }
+}
--- BaseServer/BaseServer.Web/Modules/FrontPage/FrontPagePage.cs
+++ BaseServer/BaseServer.Web/Modules/FrontPage/FrontPagePage.cs
@@ -2,10 +2,15 @@
 namespace BaseServer.FrontPage.Pages
 {
     using BaseServer.Common;
+    using RestSharp;
     using Serenity;
     using Serenity.Data;
     using System;
     using System.Web.Mvc;
+    using Hepers;
+    using System.Xml.Serialization;
+    using Newtonsoft.Json;
+    using BaseServer.FrontPage.Model;
 
     [RoutePrefix("Front"), Route("{action=index}")]
     public class FrontPageController : Controller
@@ -13,7 +18,28 @@ namespace BaseServer.FrontPage.Pages
         [HttpGet,Route("~/")]
         public ActionResult Index()
         {
-            return View(MVC.Views.FrontPage.FrontPage_, new DashboardPageModel());
+            string ip = Request.UserHostAddress;
+            if(ip== "::1")
+             ip = "181.229.0.54";
+            var clientip = new RestClient($"http://ip-api.com/json/{ip}");
+            var requestip = new RestRequest(Method.GET);
+            IRestResponse responseip = clientip.Execute(requestip);       
+            Hepers.IpResponse ipResponse = JsonConvert.DeserializeObject<Hepers.IpResponse>(responseip.Content);
+            //Wheather
+            var url = $"http://api.openweathermap.org/data/2.5/weather?q={ipResponse.city},{ipResponse.countryCode}&APPID=2e1487498cb7f439f5dad79bbc59033a&mode=json&units=metric";
+            var urllat = "http://api.openweathermap.org/data/2.5/weather?lat={ipResponse.lat}&lon={ipResponse.lon}&APPID=2e1487498cb7f439f5dad79bbc59033a&mode=json&units=metric";
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            Hepers.WheatherResponse wheatherResponse = JsonConvert.DeserializeObject<Hepers.WheatherResponse>(response.Content);
+            wheatherResponse.weather[0].icon = $"http://openweathermap.org/img/wn/{wheatherResponse.weather[0].icon}.png";
+            FrontPageModel model = new FrontPageModel()
+            {
+                IpResponse = ipResponse,
+                WheatherResponse = wheatherResponse
+
+            };            
+            return View(MVC.Views.FrontPage.FrontPage_, model);
         }
     }
 }
