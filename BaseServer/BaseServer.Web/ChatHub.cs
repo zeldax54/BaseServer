e8e997e58cde1682f55f9225aa509d695d63e0ae
Hector <hcruzada@optimalblue.com>
2020-08-30 13:18:36 -0500

    store chat in database

--- BaseServer/BaseServer.Web/ChatHub.cs
+++ BaseServer/BaseServer.Web/ChatHub.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
-
+using Serenity.Data;
 namespace BaseServer
 {
     public class ChatHub : Hub
@@ -15,6 +15,17 @@ namespace BaseServer
         public void Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
+            try
+            {
+                string ip = Context.Request.Environment["server.RemoteIpAddress"].ToString();
+                var connection = SqlConnections.NewByKey("Default");
+                connection.Query($"Insert into Chat (Ip,DateTime,Name,Msj) values('{ip}','{DateTime.Now.ToString("yyyyMMdd hh:mm:ss")}','{name}','{message}')");               
+
+            }
+            catch (Exception e) { 
+            
+            }
+           
             Clients.All.broadcastMessage(name, message);
         }
     }
---
+++ BaseServer/BaseServer.Web/Migrations/DefaultDB/DefaultDB_20200830_004500_Chats.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace BaseServer.Migrations.DefaultDB
+{
+    [Migration(20200830004500)]
+    public class DefaultDB_20200830_004500_Chats : AutoReversingMigration
+    {
+        public override void Up()
+        {
+                Create.Table("Chat").WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity().
+                WithColumn("Ip").AsString(100).NotNullable().
+                WithColumn("DateTime").AsDateTime().Nullable().
+                WithColumn("Name").AsString(100).
+                WithColumn("Msj").AsString();
+
+        }
+    }
+}
